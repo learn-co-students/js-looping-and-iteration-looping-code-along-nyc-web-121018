d72050045bde7ea5c4ab375e12e74ec6528ffa71
diaaanek <dkorongy@gmail.com>
2019-01-27 16:46:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+//
+// for ([initialization]; [condition]; [iteration]) {
+//   [loop body]
+// }
+// As soon as the condition evaluates to false, exit the loop.
+//
+const arr = ['Lisa', 'Kaitlin', 'Jan'];
+
+function printBadges(arr){
+  for (let i = 0; i < arr.length; i++){
+      console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`);
+  }
+  return arr;
+}
+
+//
+// while ([condition]) {
+//   [loop body]
+// }
+
+function tailsNeverFails(){
+  let tails = 0;
+  while (Math.random() >= 0.5){
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;// console.log(`You got ${2} tails in a row!`);
+}
