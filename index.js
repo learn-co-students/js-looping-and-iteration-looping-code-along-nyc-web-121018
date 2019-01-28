0390eba27db349491d5aa24509fbb1696cad3555
marcusplenty <marcusplenty@gmail.com>
2019-01-28 03:07:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function printBadges(names){
+  for(let i = 0; i<names.length; i++){
+    console.log("Welcome " + names[i] +"! You are employee #" + (i+1) + ".")
+  }
+  return names
+}
+
+
+function tailsNeverFails(){
+  let flipping = true;
+  let counter = 0;
+  while (flipping == true){
+    let coin = Math.random()
+    if (coin >= .5){
+      counter++;
+    }else {
+      flipping = false;
+
+    }
+  }
+  return "You got " + counter + " tails in a row!"
+}
